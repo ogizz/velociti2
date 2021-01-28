3c60386325d522c420796f65e68aca7e53697e74
ogizz <root@allyan-desktop.(none)>
2021-01-29 02:13:05 +0530

    Updates

---
+++ precache-manifest.1ec5d31eed2a29aacfa64a3a828638aa.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f32b06071ba9b9f3f3d794a2940c56",
+    "url": "/velociti2/index.html"
+  },
+  {
+    "revision": "9d5e50495f02d3a9801b",
+    "url": "/velociti2/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "cde41e157dfc4d86fc69",
+    "url": "/velociti2/static/css/main.bd99e8fb.chunk.css"
+  },
+  {
+    "revision": "9d5e50495f02d3a9801b",
+    "url": "/velociti2/static/js/2.ee37925d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/velociti2/static/js/2.ee37925d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a16977cae9c9b35209ec",
+    "url": "/velociti2/static/js/3.f2d63a20.chunk.js"
+  },
+  {
+    "revision": "cde41e157dfc4d86fc69",
+    "url": "/velociti2/static/js/main.8d43553b.chunk.js"
+  },
+  {
+    "revision": "44d30661cd4bb1f06ce9",
+    "url": "/velociti2/static/js/runtime-main.491e6b23.js"
+  },
+  {
+    "revision": "d8a22ff17917d05ab8c8dbb1e4980939",
+    "url": "/velociti2/static/media/backtouchdown.d8a22ff1.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/velociti2/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/velociti2/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/velociti2/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "7812184c3710d9623c684956fe048ddd",
+    "url": "/velociti2/static/media/fog-low.7812184c.png"
+  },
+  {
+    "revision": "fc69440e043436c18385ed0c25187b8f",
+    "url": "/velociti2/static/media/hondacity.fc69440e.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/velociti2/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/velociti2/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/velociti2/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/velociti2/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/velociti2/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/velociti2/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "678f2a0685f560e6f8c3faf23f5b9107",
+    "url": "/velociti2/static/media/vehicles.678f2a06.jpg"
+  }
+]);
